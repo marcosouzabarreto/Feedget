d49ec6a8dd2c9409e3a4d4b43d6d16538d153c2a
Marco Barreto <marcosouzabarreto@gmail.com>
2022-05-03 00:06:45 -0300

    Primeira aula

---
+++ web/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ['./src/**/*.tsx', './index.html'],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          500: '#8257e6',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
